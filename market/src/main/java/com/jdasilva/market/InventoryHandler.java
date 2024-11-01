d6cb7532f103c7c0123f619883629c1fbcc5ac47
johnconh <sevenballs@gmail.com>
2024-11-01 13:35:21 +0100

    market actualizado

--- market/src/main/java/com/jdasilva/market/InventoryHandler.java
+++ market/src/main/java/com/jdasilva/market/InventoryHandler.java
@@ -8,14 +8,9 @@ public class InventoryHandler extends BaseHandler {
     @Override
     public void handle(String message, int clientId, PrintWriter out, Map<String, Integer> inventory) {
         Map <String, String> fields = parseFIXMessage(message);
-        String action = fields.get("35");
         String instrument = fields.get("55");
         int quantity = Integer.parseInt(fields.get("38"));
-        if("D".equals(action)){
-            inventory.put(instrument, inventory.getOrDefault(instrument, 0) + quantity);
-        } else if("F".equals(action)){
-            inventory.put(instrument, inventory.getOrDefault(instrument, 0) - quantity);
-        }
+        inventory.put(instrument, quantity);
         super.handle(message, clientId, out, inventory);
     }
 
